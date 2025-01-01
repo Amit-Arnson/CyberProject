9ab938d14e2de0205bf1f8ac00471f100a550014
amit <amitarnson2007@gmail.com>
2025-01-01 18:02:54 +0200

    started work on endpoint checking (validating valid method and authentication requirements per endpoint)

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-import socket
 import os
 
 from dotenv import load_dotenv
 from Caches.user_cache import UserCache
 from Caches.client_cache import Address, ClientPackage
+
+from dataclasses import dataclass
 
 import asyncio
 from asyncio import transports
@@ -19,19 +20,37 @@ PORT = 5555
 cached_authorization = UserCache()
 
 
-async def connection(client_pack: ClientPackage):
-    event_loop = asyncio.get_event_loop()
+class EndPoints:
+    # these are the paths the sever gets from a client.
+    # each path will have a method (get/post) and if it requires a valid session token
+    @dataclass
+    class EndPointRequires:
+        # which method it requires
+        method: str
+
+        # if it requires authentication beforehand
+        authentication: bool
+
+        def __post_init__(self):
+            self.method = self.method.lower()
+
+    def __init__(self):
+        # these are the endpoints where the client sends to the server
+        self.endpoints = {
+            "authentication/dhe": self.EndPointRequires(method="get", authentication=False),
+            "authentication/aes": self.EndPointRequires(method="get", authentication=False)
+        }
 
 
+# note that read/write using asyncio's protocol adds its own buffer, so we don't need to manually add one.
 class ServerProtocol(asyncio.Protocol):
     def __init__(self):
         self.client_package: ClientPackage | None = None
 
-    def connection_made(self, transport: transports.BaseTransport) -> None:
+    def connection_made(self, transport: transports.Transport) -> None:
         # gets the ip-port pair as a tuple
         client_address = transport.get_extra_info("peername")
-
-        address = Address(client_address)
+        address = Address(ip_port_tuple=client_address)
         client_information = ClientPackage(
             address=address,
             client=transport,
@@ -41,19 +60,11 @@ class ServerProtocol(asyncio.Protocol):
             self.client_package = client_information
 
     def data_received(self, data: bytes) -> None:
-        pass
-
+        print(data.decode())
 
 
 async def main() -> None:
     """Hosts a server to communicate with a client through sending and receiving string data."""
-
-    # # Creates the connection object.
-    # server = socket.socket()
-    # # Binds the connection to an address.
-    # server.bind((IP, PORT))
-    #
-    # server.setblocking(False)
 
     event_loop = asyncio.get_event_loop()
 
@@ -65,26 +76,6 @@ async def main() -> None:
 
     async with server:
         await server.serve_forever()
-
-    # while True:
-    #     try:
-    #         # Listens to clients trying to connect.
-    #         server.listen()
-    #
-    #         # Accepts a connection with one client and creates its connection object.
-    #         client, client_address = await event_loop.sock_accept(server)
-    #
-    #         client_address = Address(client_address)
-    #
-    #         client_information = ClientPackage(
-    #             address=client_address,
-    #             client=client,
-    #         )
-    #
-    #         event_loop.create_task(connection(client_information))
-    #     except Exception as e:
-    #         print(f"in main: {e}")
-
 
 if __name__ == "__main__":
     asyncio.run(main())
