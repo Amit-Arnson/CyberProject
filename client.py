0960c7196c8bbe81209cc5d8d3cd114d64d923a6
amit <amitarnson2007@gmail.com>
2025-01-01 18:00:07 +0200

    built async client

--- client.py
+++ client.py
@@ -1,16 +1,23 @@
 import asyncio
 from asyncio import transports
 
+
 # The IP and PORT of the server.
 IP = "127.0.0.1"
 PORT = 5555
 
+
+# note that read/write using asyncio's protocol adds its own buffer, so we don't need to manually add one.
 class ClientProtocol(asyncio.Protocol):
-    def connection_made(self, transport: transports.BaseTransport) -> None:
+    def __init__(self):
+        self.transport: transports.Transport | None = None
+
+    def connection_made(self, transport: transports.Transport) -> None:
+        self.transport = transport
         pass
 
     def connection_lost(self, exc: Exception | None) -> None:
-        pass
+        self.transport.close()
 
     def data_received(self, data: bytes) -> None:
         pass
@@ -19,19 +26,11 @@ class ClientProtocol(asyncio.Protocol):
 async def main():
     event_loop = asyncio.get_event_loop()
 
-    on_con_lost = event_loop.create_future()
-
     transport, protocol = await event_loop.create_connection(
         ClientProtocol,
-        host=IP, port=PORT)
-
-    # Wait until the protocol signals that the connection
-    # is lost and close the transport.
-    try:
-        await on_con_lost
-    finally:
-        transport.close()
-
+        host=IP,
+        port=PORT
+    )
 
 if __name__ == "__main__":
     asyncio.run(main())
