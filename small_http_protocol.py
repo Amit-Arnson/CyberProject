db1f2395f9ea4c1823a07a6fd2890fee78bd9a2c
aa <n@nin>
2025-01-01 20:42:28 +0200

    finished ServerMessage class. added __str__, encode() and decode() methods as well as checks for valid structure of the class in the __post_init__.

--- small_http_protocol.py
+++ small_http_protocol.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass
 from typing import Any
 
@@ -9,29 +10,72 @@ methods = [
 ]
 
 
+class InputError(Exception):
+    pass
+
+
 @dataclass
 class ServerMessage:
     """
     status: {"code": int, "message": str}
     method: post | get | respond | initiate
-    route: endpoint
+    endpoint: endpoint
     payload: json
     """
-    status: dict[str, int | str]
-    method: str
-    route: str
-    payload: dict[str, Any]
+    status: dict[str, int | str] = None
+    method: str = None
+    endpoint: str = None
+    payload: dict[str, Any] = None
+
+    encoded: bytes = None
+
+    def __post_init__(self):
+        # if any of the following attributes are NOT none, it means the message isn't encoded. flipping this by adding a not
+        # creates us a variable that checks if the message was **built** as encoded
+
+        # no attribute should ever be none. due to this reason, it is safe to assume that if all of them are none it means
+        # that the message is meant to be encoded
+        has_all_attributes = any((self.status, self.method, self.endpoint, self.payload))
+        should_be_encoded = not has_all_attributes
+
+        if not self.encoded and should_be_encoded:
+            raise AttributeError(f"expected encoded bytes but got {self.encoded} ({type(self.encoded)}) instead")
 
     def dictionary(self):
         return {
-            "status": {
-                "code": self.status["code"],
-                "message": self.status["message"]
-            },
+            "status": self.status,
             "method": self.method,
-            "route": self.route,
+            "endpoint": self.endpoint,
             "payload": self.payload
         }
-    def __str__(self):
-        # testing to make sure github is linked correctly
-        pass
+
+    def encode(self) -> bytes:
+        """
+        encodes the dumped values (in a json format) and returns the bytes
+        """
+        self.encoded = self.__str__().encode()
+        return self.encoded
+
+    def decode(self) -> dict[str, Any]:
+        """
+        returns the loaded json gotten from self.encoded.
+        in addition, it replaces all the other attributes with the ones gotten from the loaded json.
+        """
+        if not self.encoded or not isinstance(self.encoded, bytes):
+            raise AttributeError(f"expected encoded bytes but got {self.encoded} ({type(self.encoded)}) instead")
+
+        decoded = self.encoded.decode()
+        loaded_json = json.loads(decoded)
+
+        self.status = loaded_json.get("status")
+        self.endpoint = loaded_json.get("endpoint")
+        self.method = loaded_json.get("method")
+        self.payload = loaded_json.get("payload")
+
+        return loaded_json
+
+    def __str__(self) -> str:
+        """
+        returns a dumped json of all the values (with regard for ascii)
+        """
+        return json.dumps(self.dictionary(), ensure_ascii=False)
