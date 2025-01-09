7aa7d68ca10505a9ed282deab3bc6dc875d15c2c
aa <n@nin>
2025-01-09 21:14:14 +0200

    added the user_cache reference to the server actions

--- server_actions.py
+++ server_actions.py
@@ -1,7 +1,13 @@
 from pseudo_http_protocol import ClientMessage
 from Caches.client_cache import ClientPackage
+from Caches.user_cache import UserCache, UserCacheItem
+
+from AES_128 import cbc
 
 
-async def authenticate_client(client_package: ClientPackage, client_message: ClientMessage):
+async def authenticate_client(client_package: ClientPackage, client_message: ClientMessage, user_cache: UserCache):
     client = client_package.client
-    print(client_package.address.ip + " :)")
+
+    client_session_token = client_message.authentication
+    client_user_cache: UserCacheItem = user_cache[client_session_token]
+
