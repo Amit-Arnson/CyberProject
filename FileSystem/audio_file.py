c93b7ef7f69ecf8b74eacc563a09c959d5149e3b
aa <n@nin>
2025-01-09 15:41:24 +0200

    added AudioFile which inherits from BaseFile. This class limits files to certain extensions and sizes.

---
+++ FileSystem/audio_file.py
@@ -0,0 +1,32 @@
+from base_file_system import BaseFile
+
+
+class AudioFile(BaseFile):
+    """a child class of BaseFile designed to limit files to certain extensions and size"""
+    def __init__(self, file_path: str):
+        super().__init__(file_path=file_path)
+        self._allowed_extensions = (
+            "wav", "mp3", "ogg"
+        )
+
+        # 10 MB
+        self._max_size = 10000000
+
+    def allowed_size(self) -> bool:
+        """checks if the size of the file is less than the max file size that is allowed"""
+        return len(self) <= self._max_size
+
+    def allowed_extension(self) -> bool:
+        """checks if the file's extension exists in the allowed extensions"""
+        return self.file_extension in self._allowed_extensions
+
+    def is_allowed(self) -> bool:
+        """
+        checks if the file adheres to the size and extension limitations
+        """
+        return all(
+            (
+             self.allowed_size(),
+             self.allowed_extension()
+            )
+        )
