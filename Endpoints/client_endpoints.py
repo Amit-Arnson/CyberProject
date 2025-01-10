10f237c3ba2a1f10779135ca47570cd09082ac26
amit <amitarnson2007@gmail.com>
2025-01-10 11:00:52 +0200

    created client endpoint class

---
+++ Endpoints/client_endpoints.py
@@ -0,0 +1,26 @@
+from typing import Callable
+from client_actions import complete_authentication
+
+
+class EndPoints:
+    def __init__(self):
+        self.endpoints: dict[str, Callable] = {
+            "authentication/key_exchange": complete_authentication
+        }
+        # endpoint -> function
+
+    def __getitem__(self, item: str):
+        """
+        returns the function related to the endpoint
+
+        :param item: endpoint name
+        """
+        return self.endpoints[item]
+
+    def __contains__(self, item: str):
+        """
+        checks if an endpoint exists in the client
+
+        :param item: endpoint name
+        """
+        return item in self.endpoints
