5498cded18c3552ce14090a2494cf72291055e57
Amit Arnson <amitarnson2007@gmail.com>
2025-01-11 00:10:09 +0200

    created a transport class based on asyncio.Transport which supports aes-128-cbc encrypting

---
+++ encryptions.py
@@ -0,0 +1,87 @@
+import asyncio
+from typing import Optional
+
+from AES_128 import cbc
+
+
+class EncryptedTransport(asyncio.Transport):
+    def __init__(self, transport: asyncio.Transport, key: Optional[bytes] = None, iv: Optional[bytes] = None):
+        super().__init__()
+        self._transport = transport
+        self.key = key
+        self.iv = iv
+
+        if key and len(key) != 16:
+            raise ValueError(f"expected 16 byte key, got {len(key)} bytes instead")
+
+        if key and len(iv) != 16:
+            raise ValueError(f"expected 16 byte IV, got {len(iv)} bytes instead")
+
+    def write(self, data: bytes) -> None:
+        """
+        uses asyncio.Transport's write() whilst also encrypting using AES-128-CBC.
+        data is only encrypted if a key and iv are passed
+        """
+        if self.key and self.iv:
+            data = cbc.cbc_encrypt(data, key=self.key, iv=self.iv)
+
+        self._transport.write(data)
+
+    def read(self, data: bytes) -> bytes:
+        """
+        decrypts the data using the key and iv that were initially passed.
+        data is only decrypted if a key and iv are passed
+        """
+
+        if self.key and self.iv:
+            data = cbc.cbc_decrypt(data, key=self.key, iv=self.iv)
+
+        return data
+
+    def can_write_eof(self) -> bool:
+        """
+        Returns whether the transport can send an EOF (end of file).
+        """
+        return self._transport.can_write_eof()
+
+    def write_eof(self) -> None:
+        """
+        Writes an EOF (end of file) marker to the transport.
+        """
+        self._transport.write_eof()
+
+    def get_extra_info(self, name: str, default: Optional[None] = None) -> Optional[None]:
+        """
+        Returns extra information about the transport (such as peername, sockname, etc.).
+        """
+        return self._transport.get_extra_info(name, default)
+
+    def close(self) -> None:
+        """
+        Closes the underlying transport.
+        """
+        self._transport.close()
+
+    def is_closing(self) -> bool:
+        """
+        Checks if the transport is closing.
+        """
+        return self._transport.is_closing()
+
+    def set_protocol(self, protocol: asyncio.Protocol) -> None:
+        """
+        Sets the protocol that the transport is associated with.
+        """
+        self._transport.set_protocol(protocol)
+
+    def get_write_buffer_size(self) -> int:
+        """
+        Returns the current write buffer size of the transport.
+        """
+        return self._transport.get_write_buffer_size()
+
+    def __del__(self):
+        """
+        Cleanup when the object is deleted.
+        """
+        self.close()
