570e37acf7e7f65b5dece643d53ae26c332b1ca9
amit <amitarnson2007@gmail.com>
2025-01-17 11:05:11 +0200

    added fetch_user to get user info

--- queries.py
+++ queries.py
@@ -5,8 +5,14 @@ from asqlite import ProxiedConnection
 
 class User:
     @staticmethod
-    async def create_user(connection: ProxiedConnection, user_id, username: str, display_name: str, password: str, salt: bytes) -> None:
+    async def create_user(connection: ProxiedConnection, user_id: str, username: str, display_name: str, password: str, salt: bytes) -> None:
         await connection.execute(
             "INSERT INTO users (user_id, username, display_name, password, salt) VALUES (?, ?, ?, ?, ?)",
             user_id, username, display_name, password, salt
         )
+
+    # todo: find out what the Row is
+    @staticmethod
+    async def fetch_user(connection: ProxiedConnection, username: str):
+        row = await connection.fetchone("SELECT * FROM users WHERE username = ?", username)
+        return row["password"], row["salt"]
