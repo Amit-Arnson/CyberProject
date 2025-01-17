19d41802f146d3f9a3ce962d250122ec2bb18c4d
amit <amitarnson2007@gmail.com>
2025-01-17 09:09:16 +0200

    added a function to create the user table

--- initiate_database.py
+++ initiate_database.py
@@ -1,6 +1,8 @@
 import asqlite
 import asyncio
 
+from asqlite import Cursor
+
 
 class CreateTables:
     def __init__(self, database: str):
@@ -10,13 +12,14 @@ class CreateTables:
         async with asqlite.connect(self.database) as connection:
             async with connection.cursor() as cursor:
                 await self._create_clusters(cursor)
+                await self._create_users(cursor)
                 # ...
 
                 await connection.commit()
 
     # all of these methods will create tables
     @staticmethod
-    async def _create_clusters(cursor: asqlite.Cursor):
+    async def _create_clusters(cursor: Cursor):
         await cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS clusters (
@@ -24,6 +27,21 @@ class CreateTables:
                 max_size INTEGER NOT NULL,
                 current_size INTEGER NOT NULL DEFAULT 0
             );
+            """
+        )
+
+    @staticmethod
+    async def _create_users(cursor: Cursor):
+        print(type(cursor))
+        await cursor.execute(
+            """
+                        CREATE TABLE IF NOT EXISTS users (
+                            user_id INTEGER PRIMARY KEY,
+                            username TEXT NOT NULL,
+                            display_name TEXT NOT NULL,
+                            password TEXT NOT NULL,
+                            salt BLOB NOT NULL
+                        );
             """
         )
 
