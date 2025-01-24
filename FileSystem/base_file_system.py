2b833e2fa97ecd9ea3c9f3691ca865f69f3d9823
amit <amitarnson2007@gmail.com>
2025-01-24 11:01:03 +0200

    started working on a way to save files using the FileSystem that allows files to be split into clusters.

--- FileSystem/base_file_system.py
+++ FileSystem/base_file_system.py
@@ -1,14 +1,12 @@
 import os
 import asyncio
+import time
+
 import aiofiles
 import asqlite
 from FileSystem.file_extension import Extension
 
-
-class InvalidExtension(Exception):
-    """Use this class to throw errors when an unacceptable extension is used"""
-    def __init__(self):
-        super().__init__("Invalid file extension")
+from queries import FileSystem
 
 
 class System:
@@ -17,9 +15,111 @@ class System:
     using those names.
     if you are unsure, do not pass any arguments unless they are required.
     """
-    def __init__(self, directory: str = "audio_files", cluster_size: int = 100):
+    def __init__(self, db_pool: asqlite.Pool, directory: str = "audio_files", cluster_size: int = 100):
+        self.db_pool = db_pool
+
         os.makedirs(directory, exist_ok=True)
+
+        self.main_directory = directory
         self.cluster_size = cluster_size
+
+    @staticmethod
+    def _create_unique_id(random_bytes_length: int = 8) -> str:
+        # to ensure a unique ID, we can use the current time with randomized bytes at the end
+        current_time_hex = time.time().hex()
+
+        random_bytes_hex = os.urandom(random_bytes_length).hex()
+
+        # we hex both of them since cluster ID is a string that is basically a dir
+
+        unique_cluster_id = f"{current_time_hex}_{random_bytes_hex}"
+
+        return unique_cluster_id
+
+    async def _create_file_id(self) -> str:
+        """:returns: a unique file ID that doesnt exist in the database yet"""
+
+        unique_file_id = ""
+        exists = True
+        while exists:
+
+            unique_file_id = self._create_unique_id()
+
+            async with self.db_pool.acquire() as connection:
+                exists = FileSystem.does_file_exist(connection=connection, file_id=unique_file_id)
+
+        # this error should NEVER raise. if it does, fuck.
+        if not unique_file_id:
+            raise Exception("new achievement: how did we get here?")
+
+        return unique_file_id
+
+    async def _create_cluster_id(self) -> str:
+        """:returns: a unique cluster ID that doesnt exist in the database yet"""
+
+        unique_cluster_id = ""
+        exists = True
+        while exists:
+
+            unique_cluster_id = self._create_unique_id()
+
+            async with self.db_pool.acquire() as connection:
+                exists = FileSystem.does_cluster_exist(connection=connection, cluster_id=unique_cluster_id)
+
+        # this error should NEVER raise. if it does, fuck.
+        if not unique_cluster_id:
+            raise Exception("new achievement: how did we get here?")
+
+        return unique_cluster_id
+
+    async def _create_new_cluster(self) -> str:
+        """:returns: name (ID) of the cluster"""
+
+        # 1) generate cluster ID
+        cluster_id = await self._create_cluster_id()
+
+        # 2) save cluster
+        async with self.db_pool.acquire() as connection:
+            await FileSystem.create_new_cluster(connection=connection, cluster_id=cluster_id)
+
+        # 3) create cluster under dir (main_dir/cluster_id)
+        cluster_dir = os.path.join(self.main_directory, cluster_id)
+
+        # creates the cluster dir under the parent directory.
+        os.makedirs(cluster_dir)
+
+        # 4) return cluster ID
+        return cluster_dir
+
+    async def _find_free_cluster(self) -> str:
+        """:returns: cluster's full directory"""
+
+        async with self.db_pool.acquire() as connection:
+            free_cluster_id = FileSystem.find_free_cluster(connection, max_size=self.cluster_size)
+
+        if not free_cluster_id:
+            free_cluster_id = await self._create_new_cluster()
+
+        return os.path.join(self.main_directory, free_cluster_id)
+
+    # todo: create User object and change upload ID to User Object (which will contain ID, along other things)
+    async def save(self, file: "BaseFile", uploaded_by_id: str) -> tuple[str, str]:
+        """
+        accepts a file and saves it to an available cluster.
+        a file will be saved under "directory"/"file ID", as well as in the database under the "files" table (base case for files)
+
+        to save the file to an image/audio file table, it must be done manually.
+
+        :param file: the file that you want to save
+        :param uploaded_by_id: the User ID that uploaded the file
+        :returns: tuple[file ID (name), saved directory (main_dir/cluster_id)]
+        """
+
+        # finds a free cluster's directory path. if a free cluster does not exist, it creates on.
+        save_under = await self._find_free_cluster()
+
+        # TODO: finish working on save
+        # todo: see if maybe automatic save to image/audio file tables. check doc-string for reference
 
 
 # main_dir/cluster_id/file_id
@@ -64,12 +164,13 @@ class BaseFile:
 
     async def save(self, name: str, path: str) -> str:
         """
-        :param name: the name (or ID) of the file
+        :param name: the name (or ID) of the file. it will replace any current file name with the given one
         :param path: the directory where you want it saved
         :return: the full path of the file
         """
 
         # combines the name (ID) of the file with the directory it should be saved under
+        # main_dir/cluster_id/file_name
         path = os.path.join(path, name)
 
         if not self._file:
