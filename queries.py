160a3dd42ff9a0686ddb74ea6465c676cb7cbc17
amit <amitarnson2007@gmail.com>
2025-01-24 11:00:25 +0200

    added FileSystem along with the functions to manage clusters (+ started working on functions to manage the files themselves) in the database.

--- queries.py
+++ queries.py
@@ -1,4 +1,5 @@
 # this file is used as a way to organize all the needed SQL queries
+import time
 
 from asqlite import ProxiedConnection
 from sqlite3 import Row
@@ -6,7 +7,8 @@ from sqlite3 import Row
 
 class User:
     @staticmethod
-    async def create_user(connection: ProxiedConnection, user_id: str, username: str, display_name: str, password: str, salt: bytes) -> None:
+    async def create_user(connection: ProxiedConnection, user_id: str, username: str, display_name: str, password: str,
+                          salt: bytes) -> None:
         await connection.execute(
             "INSERT INTO users (user_id, username, display_name, password, salt) VALUES (?, ?, ?, ?, ?)",
             user_id, username, display_name, password, salt
@@ -22,6 +24,68 @@ class User:
         user = await User.fetch_user(connection=connection, username=username)
 
         if user:
+            return True
+
+        return False
+
+
+class FileSystem:
+    # clusters
+    @staticmethod
+    async def find_free_cluster(connection: ProxiedConnection, max_size: int) -> str | None:
+        """:returns: the cluster's name (ID). find files with main_dir/cluster_id/file_id"""
+
+        row = await connection.fetchone("SELECT cluster_id FROM clusters WHERE current_size < ?", max_size)
+
+        if row:
+            return row["cluster_id"]
+
+        return None
+
+    @staticmethod
+    async def does_cluster_exist(connection: ProxiedConnection, cluster_id: str) -> bool:
+        """:returns: whether the cluster exists or not (True means it exists)"""
+
+        cluster = await connection.fetchone("SELECT * FROM clusters WHERE cluster_id = ?", cluster_id)
+
+        if cluster:
+            return True
+
+        return False
+
+    @staticmethod
+    async def create_new_cluster(connection: ProxiedConnection, cluster_id: str) -> None:
+        # default for current_size is 0, so we don't need to set it.
+        await connection.execute("INSERT INTO clusters (cluster_id) VALUES (?)", cluster_id)
+
+    # files
+
+    @staticmethod
+    async def create_base_file(
+            connection: ProxiedConnection,
+            cluster_id: str,
+            file_id: str,
+            user_uploaded_id: str,
+            size: int,
+    ) -> None:
+
+        current_time = int(time.time())
+
+        await connection.execute(
+            """
+            INSERT INTO files
+            (file_id, file_cluster_id, user_uploaded_id, size, uploaded_at) VALUES (?, ?, ?, ?, ?)
+            """,
+            file_id, cluster_id, user_uploaded_id, size, current_time
+        )
+
+    @staticmethod
+    async def does_file_exist(connection: ProxiedConnection, file_id: str) -> bool:
+        """:returns: whether the file exists or not (True means it exists)"""
+
+        file = await connection.fetchone("SELECT * FROM files WHERE file_id = ?", file_id)
+
+        if file:
             return True
 
         return False
