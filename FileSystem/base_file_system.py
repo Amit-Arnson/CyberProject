10eec2fd12708216dcf1e62fbc4ec333f264c287
Amit Arnson <amitarnson2007@gmail.com>
2025-01-25 17:43:56 +0200

    finished the .save() function for the System class.
    added a setter function for max_cluster_size and default_directory instead of providing them in the __init__.

--- FileSystem/base_file_system.py
+++ FileSystem/base_file_system.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import asyncio
 import time
@@ -10,18 +11,32 @@ from queries import FileSystem
 
 
 class System:
-    """
-    if you change the default names of things in this class, make sure to ALWAYS build the class
-    using those names.
-    if you are unsure, do not pass any arguments unless they are required.
-    """
-    def __init__(self, db_pool: asqlite.Pool, directory: str = "audio_files", cluster_size: int = 100):
+    def __init__(self, db_pool: asqlite.Pool):
         self.db_pool = db_pool
 
-        os.makedirs(directory, exist_ok=True)
+        # setting default values inside the __init__ so that its clear that these shouldn't just be chosen randomly.
+        # to change their value, check the set_... functions.
+        self._main_directory = "AudioFiles"
+        self._cluster_size = 100
 
-        self.main_directory = directory
-        self.cluster_size = cluster_size
+    # I have created set_... for directory and cluster size so that it is clear that these values are not "just" part of the constructor.
+    # changing these values means that the files will be saved in different places, and that the max files saved in one place will be changed.
+    # if that is the behaviour you want, you can use these functions to change the values.
+    def set_default_directory(self, directory: str):
+        """set the main directory to one of your choosing. this is where the clusters will be created"""
+        self._main_directory = directory
+
+    def set_max_cluster_size(self, max_cluster_size: int):
+        """set the maximum amount of files that can be saved under each cluster"""
+        self._cluster_size = max_cluster_size
+
+    # we don't want it attempting to create directories just by calling the class, so I made it into a separate function
+    def initialize(self):
+        """create the directory which every file will be saved under"""
+
+        # only attempt to make the directory if it doesn't exist yet
+        if not os.path.exists(self._main_directory):
+            os.makedirs(self._main_directory, exist_ok=True)
 
     @staticmethod
     def _create_unique_id(random_bytes_length: int = 8) -> str:
@@ -36,6 +51,7 @@ class System:
 
         return unique_cluster_id
 
+    # todo: i might want to add the user_upload_id to the _create_file_id and therefore for now it is a different function than _create_cluster_id
     async def _create_file_id(self) -> str:
         """:returns: a unique file ID that doesnt exist in the database yet"""
 
@@ -83,7 +99,7 @@ class System:
             await FileSystem.create_new_cluster(connection=connection, cluster_id=cluster_id)
 
         # 3) create cluster under dir (main_dir/cluster_id)
-        cluster_dir = os.path.join(self.main_directory, cluster_id)
+        cluster_dir = os.path.join(self._main_directory, cluster_id)
 
         # creates the cluster dir under the parent directory.
         os.makedirs(cluster_dir)
@@ -91,16 +107,16 @@ class System:
         # 4) return cluster ID
         return cluster_dir
 
-    async def _find_free_cluster(self) -> str:
-        """:returns: cluster's full directory"""
+    async def _find_free_cluster(self) -> tuple[str, str]:
+        """:returns: tuple[cluster's full directory (main_dir/cluster_id), cluster ID]"""
 
         async with self.db_pool.acquire() as connection:
-            free_cluster_id = FileSystem.find_free_cluster(connection, max_size=self.cluster_size)
+            free_cluster_id = FileSystem.find_free_cluster(connection, max_size=self._cluster_size)
 
         if not free_cluster_id:
             free_cluster_id = await self._create_new_cluster()
 
-        return os.path.join(self.main_directory, free_cluster_id)
+        return os.path.join(self._main_directory, free_cluster_id), free_cluster_id
 
     # todo: create User object and change upload ID to User Object (which will contain ID, along other things)
     async def save(self, file: "BaseFile", uploaded_by_id: str) -> tuple[str, str]:
@@ -116,13 +132,31 @@ class System:
         """
 
         # finds a free cluster's directory path. if a free cluster does not exist, it creates on.
-        save_under = await self._find_free_cluster()
+        save_directory, cluster_id = await self._find_free_cluster()
 
-        # TODO: finish working on save
+        # create a random file ID to save the file under
+        file_id = await self._create_file_id()
+
+        try:
+            # saves the file under main_dir/cluster_dir/file_id
+            await file.save(name=file_id, path=save_directory)
+        except Exception as e:
+            print(f"error: {e}: cluster ID: {cluster_id}, file ID: {file_id}, user ID: {uploaded_by_id}")
+            raise e
+
+        # save the file in the database
+        async with self.db_pool.acquire() as connection:
+            await FileSystem.create_base_file(
+                connection=connection,
+                cluster_id=cluster_id,
+                file_id=file_id,
+                user_uploaded_id=uploaded_by_id,
+                size=file.size()
+            )
+
+        return file_id, save_directory
+
         # todo: see if maybe automatic save to image/audio file tables. check doc-string for reference
-
-
-# main_dir/cluster_id/file_id
 
 
 class BaseFile:
@@ -176,9 +210,15 @@ class BaseFile:
         if not self._file:
             raise ValueError("No content to save. Load or set the file content first.")
 
-        # write bytes
-        async with aiofiles.open(path, "wb") as file:
-            await file.write(self._file)
+        try:
+            # write bytes
+            async with aiofiles.open(path, "wb") as file:
+                await file.write(self._file)
+        except Exception as e:
+            logging.error(e)
+
+            # todo: check code consistency to see if we need an error class instead of base exception. (error code: 500)
+            raise Exception(f"failed to save file \"{path}\" under file.save() (BaseFile)")
 
         return path
 
@@ -197,6 +237,10 @@ class BaseFile:
         await cls_file.load()
 
         return cls_file
+
+    def size(self):
+        """an alternative method to len(). returns the size of the file in bytes"""
+        return self.__len__()
 
     def __bytes__(self) -> bytes:
         return self._file
