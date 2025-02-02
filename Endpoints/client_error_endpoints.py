645beca8d02762e11518dd173a25c1663b688a30
Amit Arnson <amitarnson2007@gmail.com>
2025-02-02 17:42:42 +0200

    added a routing method for "error functions" (the functions that will display GUI info about errors)

---
+++ Endpoints/client_error_endpoints.py
@@ -0,0 +1,26 @@
+from typing import Callable
+from client_server_errors import login_error
+
+
+class ErrorEndPoints:
+    def __init__(self):
+        self.endpoints: dict[str, Callable] = {
+            "user/login/error": login_error,
+        }
+        # endpoint -> function
+
+    def __getitem__(self, item: str):
+        """
+        returns the function related to the endpoint
+
+        :param item: endpoint name
+        """
+        return self.endpoints[item]
+
+    def __contains__(self, item: str):
+        """
+        checks if an endpoint exists in the client
+
+        :param item: endpoint name
+        """
+        return item in self.endpoints
