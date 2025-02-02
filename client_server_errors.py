c2418122c6bb8efe616b66164d23edffcd5b3d44
Amit Arnson <amitarnson2007@gmail.com>
2025-02-02 18:05:57 +0200

    added the visual error handling for signup

--- client_server_errors.py
+++ client_server_errors.py
@@ -47,3 +47,46 @@ async def login_error(page: Page, _: EncryptedTransport, server_message: ServerM
                 f"{status_message}\n\nstatus: {status_code}"
             )
         )
+
+
+async def signup_error(page: Page, _: EncryptedTransport, server_message: ServerMessage, __: ClientSideUserCache):
+    """
+    this function is used to visually display that an error occurred when trying to sign up to create a new account.
+
+    tied to user/signup/login/error
+
+    (this is tied to user/signup/login and not use user/signup because in the GUI it will also try to instantly log the user
+    in, this means that user/signup is never actually called as a stand-alone in the GUI)
+
+    expected payload:
+    {
+        ???
+    }
+
+    expected output:
+    None
+
+    expected page location:
+    SignupPage
+    """
+
+    status_code = server_message.status.get("code")
+    status_message = server_message.status.get("message")
+
+    # since the .show() method applies "self" to page.view, we can access the page's contents and controls (which are
+    # normally saved in the self of LoginPage) by accessing page.view
+
+    if hasattr(page, "view"):
+        signup_page = page.view
+
+        # todo: check if i want to make it so that instead of directly changing the "self" values, ill have a helper function inbetween to make things more controlled
+        signup_page.username_textbox.error_text = status_message
+        signup_page.username_textbox.update()
+    else:
+        # whilst page.view may not exist in some cases, page.server_error is always defined at the start of runtime.
+        # this means that we can always access it.
+        page.server_error(
+            ft.Text(
+                f"{status_message}\n\nstatus: {status_code}"
+            )
+        )
