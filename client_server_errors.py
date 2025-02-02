1bb84fcf1c1ed97607456b63fcc4a64b22ef77be
Amit Arnson <amitarnson2007@gmail.com>
2025-02-02 17:42:16 +0200

    added functions to handle the visual display of errors from the server

---
+++ client_server_errors.py
@@ -0,0 +1,48 @@
+# this is where all the "error" endpoints will point
+
+from pseudo_http_protocol import ServerMessage
+from Caches.user_cache import ClientSideUserCache
+
+from encryptions import EncryptedTransport
+
+import flet as ft
+from flet import Page
+
+
+async def login_error(page: Page, _: EncryptedTransport, server_message: ServerMessage, __: ClientSideUserCache):
+    """
+    this function is used to visually display that an error occurred when trying to log in.
+
+    tied to user/login/error
+
+    expected payload:
+    {
+        ???
+    }
+
+    expected output:
+    None
+
+    expected page location:
+    LoginPage
+    """
+
+    status_code = server_message.status.get("code")
+    status_message = server_message.status.get("message")
+
+    # since the .show() method applies "self" to page.view, we can access the page's contents and controls (which are
+    # normally saved in the self of LoginPage) by accessing page.view
+
+    if hasattr(page, "view"):
+        login_page = page.view
+
+        login_page.username_textbox.error_text = status_message
+        login_page.username_textbox.update()
+    else:
+        # whilst page.view may not exist in some cases, page.server_error is always defined at the start of runtime.
+        # this means that we can always access it.
+        page.server_error(
+            ft.Text(
+                f"{status_message}\n\nstatus: {status_code}"
+            )
+        )
