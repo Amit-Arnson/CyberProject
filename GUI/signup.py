9c477371ebeb26294e83ecf412bb4c7760c73996
Amit Arnson <amitarnson2007@gmail.com>
2025-02-03 22:02:55 +0200

    changed the code between signup and signup2, this is so that the main code is the updated code and that i will keep a (temporary) copy of the old code

--- GUI/signup.py
+++ GUI/signup.py
@@ -1,5 +1,3 @@
-import asyncio
-
 import flet as ft
 
 from encryptions import EncryptedTransport
@@ -7,51 +5,183 @@ from Caches.user_cache import ClientSideUserCache
 
 from pseudo_http_protocol import ClientMessage
 
+
+# todo: add the logo somewhere? idk
 class SignupPage:
     def __init__(self, page: ft.Page):
         self.page = page
         self.page.padding = 0
-        self.page.on_resized = self._page_resize
         self.page.theme_mode = ft.ThemeMode.LIGHT
 
         self.transport: EncryptedTransport | None = None
         if hasattr(page, "transport"):
             self.transport: EncryptedTransport = page.transport
 
-        # the user cache is not needed in signup (since we don't have a session token assigned yet)
+        # the user cache is not needed in login (since we don't have a session token assigned yet)
         # self.user_cache: ClientSideUserCache | None = None
         # if hasattr(page, "user_cache"):
         #     self.user_cache: ClientSideUserCache = page.user_cache
 
         self._initialize_controls()
 
-    def _initialize_controls(self):
+    @staticmethod
+    def _full_border(border_color: ft.Colors):
+        border = ft.Border(
+            top=ft.BorderSide(width=1, color=border_color),
+            bottom=ft.BorderSide(width=1, color=border_color),
+            right=ft.BorderSide(width=1, color=border_color),
+            left=ft.BorderSide(width=1, color=border_color)
+        )
+
+        return border
+
+    def _initialize_username(self, border_color: ft.Colors = ft.Colors.GREY):
         self.username_textbox = ft.TextField(
             max_length=20,
             expand=True,
+            height=50,
             label="username",
+            border=ft.InputBorder.NONE,
+            content_padding=10,
+            error_style=ft.TextStyle(-10)
         )
 
+        self.username_decoration = ft.Container(
+            content=ft.Icon(ft.Icons.PERSON),
+            height=50,
+            width=50,
+            border=ft.Border(
+                right=ft.BorderSide(width=1, color=border_color),
+            ),
+        )
+
+        self.username_decoration_row = ft.Row(
+            [
+                self.username_decoration,
+                self.username_textbox
+            ],
+            spacing=0,
+            expand=True
+        )
+
+        self.username_container = ft.Container(
+            content=self.username_decoration_row,
+            border=self._full_border(border_color),
+        )
+
+    def _initialize_display_name(self, border_color: ft.Colors = ft.Colors.GREY):
         self.display_name_textbox = ft.TextField(
             max_length=20,
             expand=True,
+            height=50,
             label="display name",
+            border=ft.InputBorder.NONE,
+            content_padding=10,
+            error_style=ft.TextStyle(-10)
         )
 
+        self.display_name_decoration = ft.Container(
+            content=ft.Icon(ft.Icons.PERSON),
+            height=50,
+            width=50,
+            border=ft.Border(
+                right=ft.BorderSide(width=1, color=border_color),
+            ),
+        )
+
+        self.display_name_decoration_row = ft.Row(
+            [
+                self.display_name_decoration,
+                self.display_name_textbox
+            ],
+            spacing=0,
+            expand=True
+        )
+
+        self.display_name_container = ft.Container(
+            content=self.display_name_decoration_row,
+            border=self._full_border(border_color),
+        )
+
+    def _initialize_password(self, border_color: ft.Colors = ft.Colors.GREY):
         self.password_textbot = ft.TextField(
             password=True,
             can_reveal_password=True,
             max_length=20,
+            height=50,
             expand=True,
-            label="password"
+            label="password",
+            border=ft.InputBorder.NONE,
+            content_padding=10,
+            error_style=ft.TextStyle(-10)
         )
 
+        self.password_decoration = ft.Container(
+            content=ft.Icon(ft.Icons.LOCK),
+            height=50,
+            width=50,
+            border=ft.Border(
+                right=ft.BorderSide(width=1, color=border_color),
+            ),
+        )
+
+        self.password_decoration_row = ft.Row(
+            [
+                self.password_decoration,
+                self.password_textbot
+            ],
+            spacing=0,
+            expand=True
+        )
+
+        self.password_container = ft.Container(
+            content=self.password_decoration_row,
+            border=self._full_border(border_color),
+        )
+
+    def _initialize_confirm_password(self, border_color: ft.Colors = ft.Colors.GREY):
         self.confirm_password_textbox = ft.TextField(
             password=True,
             can_reveal_password=True,
             max_length=20,
+            height=50,
             expand=True,
-            label="confirm password"
+            label="confirm password",
+            border=ft.InputBorder.NONE,
+            content_padding=10,
+            error_style=ft.TextStyle(-10)
+        )
+
+        self.confirm_password_decoration = ft.Container(
+            content=ft.Icon(ft.Icons.PERSON),
+            height=50,
+            width=50,
+            border=ft.Border(
+                right=ft.BorderSide(width=1, color=border_color),
+            ),
+        )
+
+        self.confirm_password_decoration_row = ft.Row(
+            [
+                self.confirm_password_decoration,
+                self.confirm_password_textbox
+            ],
+            spacing=0,
+            expand=True
+        )
+
+        self.confirm_password_container = ft.Container(
+            content=self.confirm_password_decoration_row,
+            border=self._full_border(border_color),
+        )
+
+    def _initialize_buttons(self):
+        self.login_button = ft.Button(
+            bgcolor=ft.Colors.BLUE,
+            content=ft.Text("LOGIN", weight=ft.FontWeight.BOLD, color=ft.Colors.WHITE),
+            width=self.page.width,
+            height=self.page.height / 12,
+            on_click=self._switch_to_login
         )
 
         self.signup_button = ft.Button(
@@ -65,14 +195,7 @@ class SignupPage:
             on_click=self._send_signup
         )
 
-        self.to_login_button = ft.Button(
-            bgcolor=ft.Colors.BLUE,
-            content=ft.Text("LOGIN", weight=ft.FontWeight.BOLD, color=ft.Colors.WHITE),
-            width=self.page.width,
-            height=self.page.height / 12,
-            on_click=self._switch_to_login
-        )
-
+    def _initialize_divider(self):
         # the use of a stack here is to lay the Container that has the "or" text on TOP of the divider.
         self.horizontal_button_divider = ft.Stack(
             controls=[
@@ -82,160 +205,118 @@ class SignupPage:
                 ),
                 ft.Container(
                     content=ft.Text(
-                        "have an account?",
+                        "already have an account?",
                         color=ft.Colors.GREY,
                         weight=ft.FontWeight.BOLD,
                         size=self.page.height / 46
                     ),
                     bgcolor=self.page.bgcolor if self.page.bgcolor else ft.Colors.WHITE,
-
                     height=self.page.height / 30,
-                    width=self.page.width / 8,
-
+                    width=self.page.width / 6,
                     alignment=ft.Alignment(0, 0),
-
-                    right=self.page.width / 14,
-                    top=-5
                 )
             ],
             height=self.page.height / 25,
-            width=self.page.width / 3.5
+            alignment=ft.Alignment(0, 0)
         )
 
-        self.textbox_background_text = ft.Container(
-            content=ft.Column(
-                controls=[
-                    ft.Text("Signup", size=self.page.width / 35),
-                    ft.Text("Create your very own Jambox account!", size=self.page.width / 70),
+    def _initialize_text(self):
+        self.login_text = ft.Text("Login", size=50)
+        self.login_sub_text = ft.Text("Log in to your jambox account!", size=20, color=ft.Colors.GREY_600)
+        self.login_text_container = ft.Container(
+            ft.Column(
+                [
+                    self.login_text,
+                    self.login_sub_text
                 ],
-                spacing=0,
-            ),
-            height=self.page.height / 6,
-            padding=10
+            )
         )
 
-        self.textbox_background = ft.Container(
+    def _initialize_controls(self):
+        self._initialize_username()
+        self._initialize_display_name()
+        self._initialize_password()
+        self._initialize_confirm_password()
+
+        self._initialize_buttons()
+        self._initialize_divider()
+
+        self._initialize_text()
+
+        self.login_content = ft.Container(
             content=ft.Column(
                 controls=[
-                    self.textbox_background_text,
+                    ft.Column(
+                        [
+                            ft.Column([
+                                self.login_text_container,
 
-                    self.username_textbox,
-                    self.display_name_textbox,
-                    self.password_textbot,
-                    self.confirm_password_textbox,
+                                self.username_container,
+                                self.display_name_container,
+                                self.password_container,
+                                self.confirm_password_container,
+                            ]),
 
-                    self.signup_button,
-                    self.horizontal_button_divider,
-                    self.to_login_button,
+                            ft.Column(
+                                [
+                                    self.signup_button,
+                                    self.horizontal_button_divider,
+                                    self.login_button,
+                                ],
+                                spacing=13
+                            )
+                        ],
+                        spacing=25,
+                    ),
                 ],
                 spacing=12,
-                expand=True,
-                scroll=ft.ScrollMode.HIDDEN
+                expand=True
             ),
-            width=self.page.width / 3.5,
-            expand=True,
-            shadow=ft.BoxShadow(
-                spread_radius=2,
-                blur_radius=5,
-                color=ft.Colors.GREY,
-            ),
-            bgcolor=self.page.bgcolor if self.page.bgcolor else ft.Colors.WHITE,
+            width=self.page.width / 3,
             padding=10,
-            border_radius=5,
+            alignment=ft.Alignment(0, 0)
         )
 
-        self.logo_header = ft.Container(
-            # bgcolor=ft.Colors.BLUE_300,
-            content=ft.Image(
-                src="/JamBoxLOGO.png",
-                fit=ft.ImageFit.COVER,
-                width=100,
-                height=100,
-            ),
-            width=self.page.width,
-            height=self.page.height / 15,
-            alignment=ft.Alignment(-1, 0)
-        )
-
-        self.info_bottom = ft.Container(
-            width=self.page.width,
-            height=self.page.height,
-            # bgcolor=ft.Colors.BLUE_300,
-        )
-
-        self.page_view = ft.Column(
-            controls=[
-                self.logo_header,
-                self.textbox_background,
-                self.info_bottom,
+        self.spaced_content = ft.Column(
+            [
+                # the empty containers are there for the spacing of the items, so that the login content will be
+                # in the middle of the right side.
+                ft.Container(height=80, width=self.page.width/3),
+                self.login_content,
+                ft.Container(height=180, width=self.page.width/3, expand=True, )
             ],
-            horizontal_alignment=ft.CrossAxisAlignment(
-                ft.CrossAxisAlignment.CENTER
-            ),
-            spacing=0
+            alignment=ft.MainAxisAlignment.START,
         )
 
-    def _resize_divider(self):
-        self.horizontal_button_divider.width = self.page.width / 3.5
-        self.horizontal_button_divider.height = self.page.height / 25
+        self.right_side = ft.Container(
+            content=self.spaced_content,
+            expand=False,
+            bgcolor=ft.Colors.WHITE,
+            padding=0,
+        )
 
-        # divider: ft.Divider = self.horizontal_button_divider.controls[0]
-        text_container: ft.Container = self.horizontal_button_divider.controls[-1]
+        self.left_side = ft.Container(
+            expand=True,
+            bgcolor=ft.Colors.BLUE_900,
+            padding=0,
+        )
 
-        text_container.right = self.page.width / 14
-        text_container.width = self.page.width / 8
-        text_container.height = self.page.height / 30
+        self.page_view = ft.Row(
+            controls=[
+                self.right_side,
+                self.left_side
+            ],
+            expand=True,
+            spacing=0,
+        )
 
-        text_container_content: ft.Text = text_container.content
-
-        text_container_content.size = self.page.height / 46
-
-    def _resize_buttons(self):
-        self.to_login_button.width = self.page.width
-        self.to_login_button.height = self.page.height / 12
-
-        self.signup_button.width = self.page.width
-        self.signup_button.height = self.page.height / 12
-
-    def _resize_textbox_background(self):
-        self.textbox_background.width = self.page.width / 3.5
-
-        self.textbox_background_text.height = self.page.height / 6
-        login_text: ft.Text = self.textbox_background_text.content.controls[0]
-        info_text: ft.Text = self.textbox_background_text.content.controls[-1]
-
-        login_text.size = self.page.width / 35
-        info_text.size = self.page.width / 70
-
-    def _resize_header(self):
-        self.logo_header.width = self.page.width,
-        self.logo_header.height = self.page.height / 15
-
-        jambox_logo: ft.Image = self.logo_header.content
-        jambox_logo.width = self.page.width / 12
-        jambox_logo.height = self.page.width / 12
-
-    def _resize_bottom(self):
-        self.info_bottom.height = self.page.height
-        self.info_bottom.width = self.page.width
-
-    def _resize(self):
-        self._resize_bottom()
-        self._resize_header()
-        # self._resize_textbox()
-        self._resize_textbox_background()
-        self._resize_buttons()
-        self._resize_divider()
-
-    def _page_resize(self, e):
-        self._resize()
-        self.page.update()
-
+    # todo: fix the issue where it ruins the page view when it opens the banner
     def _raise_error_banner(self, error: ft.Control):
         """
             accepts the control which will be displayed to the user.
             this means you can style texts to contain clickable "links"
         """
+
         def close_banner(e):
             self.page.close(banner)
 
@@ -349,7 +430,6 @@ class SignupPage:
         self.page.add(self.page_view)
 
         self.page.update()
-
 
 def main(page: ft.Page):
     SignupPage(page=page).show()
