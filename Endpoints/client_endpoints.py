aa19b667f994bccd576f0a9536edb08da223254f
Amit Arnson <amitarnson2007@gmail.com>
2025-03-21 13:23:21 +0200

    added the song/download/preview related functions to the endpoints

--- Endpoints/client_endpoints.py
+++ Endpoints/client_endpoints.py
@@ -1,12 +1,26 @@
+import logging
 from typing import Callable
-from client_actions import complete_authentication, user_login
+from client_actions import (
+    complete_authentication,
+    user_login,
+    DownloadSong
+)
+
+async def test(*args):
+    print("NOT IMPLEMENTED")
+    logging.error("NOT IMPLEMENTED (download/preview)")
+    pass
 
 
 class EndPoints:
     def __init__(self):
+        download_song_state = DownloadSong()
+
         self.endpoints: dict[str, Callable] = {
             "authentication/key_exchange": complete_authentication,
             "user/login": user_login,
+            "song/download/preview": test,
+            "song/download/preview/file": download_song_state.download_preview_chunks
         }
         # endpoint -> function
 
