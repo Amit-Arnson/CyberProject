4d76fa391d49000fe34a5b6fa48ef00026b1bc9f
Amit Arnson <amitarnson2007@gmail.com>
2025-03-21 13:39:54 +0200

    created a function to gather the image bytes. right now it appends to the current page's page_view but will later be added to a specific location

--- client_actions.py
+++ client_actions.py
@@ -6,6 +6,7 @@ from Caches.user_cache import ClientSideUserCache
 from encryptions import EncryptedTransport
 from DHE.dhe import DHE, generate_dhe_response
 
+import flet as ft
 from flet import Page
 
 from gui_testing import MainPage
@@ -102,4 +103,71 @@ async def user_login(page: Page, transport: EncryptedTransport, server_message: 
     # this is a temporary MainPage for testing purposes only
     MainPage(page).start()
 
-    # todo: figure out how to log in the user client side, as in, GUI.
+
+class DownloadSong:
+    def __init__(self):
+        self.preview_image_chunks: dict[str, list[tuple[int, str]]] = {}
+        """
+        dict[
+            file_id, tuple[chunk number, b64_chunk]
+        ]
+        """
+
+    async def download_preview_chunks(self, page: Page, transport: EncryptedTransport, server_message: ServerMessage, user_cache: ClientSideUserCache):
+        """
+        this function gathers all the preview (cover art) file chunks and combines them in a list to later be shown
+        in the GUI
+
+        tied to song/download/preview/file
+
+        expected payload:
+        {
+            "chunk": str,
+            "file_id": str,
+            "chunk_number": int,
+            "is_last_chunk": bool,
+            "song_id": int
+        }
+
+        expected output:
+        None
+        """
+
+        payload = server_message.payload
+
+        try:
+            chunk: str = payload["chunk"]
+            chunk_number: int = payload["chunk_number"]
+            file_id = payload["file_id"]
+            song_id = payload["song_id"]
+            is_last_chunk: bool = payload["is_last_chunk"]
+        except KeyError:
+            raise  # todo: figure out what to do with malformed server messages (likely being faked messages)\
+
+        if file_id not in self.preview_image_chunks:
+            self.preview_image_chunks[file_id] = [(chunk_number, chunk)]
+        else:
+            self.preview_image_chunks[file_id].append((chunk_number, chunk))
+
+        if is_last_chunk:
+            b64_chunk_list: list[tuple[int, str]] = self.preview_image_chunks[file_id]
+
+            # sorts the chunks to be in the correct order
+            b64_chunk_list.sort(key=lambda x: x[0])
+
+            b64_file_bytes = "".join((b64_chunk for chunk_num, b64_chunk in b64_chunk_list))
+
+            page.view.page_view.controls.append(
+                ft.Container(
+                    ft.Image(
+                        src_base64=b64_file_bytes,
+                        fit=ft.ImageFit.FILL
+                    ),
+                    height=500,
+                    width=500,
+                    bgcolor=ft.Colors.GREY
+                )
+            )
+
+            page.update()
+
