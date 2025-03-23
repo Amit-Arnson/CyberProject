db461c810002f5a1e356aaf36835e292da5990a1
Amit Arnson <amitarnson2007@gmail.com>
2025-03-23 13:30:58 +0200

    created a compress_to_low_res_webp that is about 50% more efficient (in terms of compression) than the compress_to_webp, however it is much lower quality.
    this function is used for images that do not need a high quality, and that need to be sent a lot (like cover art)

--- Compress/images.py
+++ Compress/images.py
@@ -18,7 +18,51 @@ async def compress_to_webp(
 
     ffmpeg_values = FFmpegImage(
         codec=Codec.WEBP,
-        quality=2
+        quality=30
+    )
+
+    total_size = await compress_and_replace(
+        file_extension=file_extension,
+        compressed_extension=compressed_extension,
+        input_file=input_file,
+        directory=directory,
+        ffmpeg_flags=ffmpeg_values
+    )
+
+    return total_size, compressed_extension
+
+
+async def compress_to_low_res_webp(
+        file_extension: str,
+        directory: str,
+        input_file: str,
+) -> tuple[int, str]:
+    """
+    Compresses the input image file to a .webp output file, using libwebp as the -c:a flag, using FFmpeg asynchronously, and also replaces the old
+    uncompressed file with the new compressed file *using Compress.files.compress_and_replace*
+
+    this compression's flags reduce the size of the file drastically, but also reduces quality.
+
+    :returns: the new size of the compressed file (in bytes) and the output file's extension ("webp")
+    """
+
+    compressed_extension = "webp"
+
+    ffmpeg_values = FFmpegImage(
+        codec=Codec.WEBP,
+        quality=0
+    )
+
+    extra_compression_ffmpeg_values: dict[str, ...] = {
+        "-compression_level": 6,
+        "-lossless": 1,
+        "-map_metadata": -1,
+        "-map_chapters": -1,
+        "-preset": "drawing"
+    }
+
+    ffmpeg_values.to_dict(
+        extra=extra_compression_ffmpeg_values
     )
 
     total_size = await compress_and_replace(
