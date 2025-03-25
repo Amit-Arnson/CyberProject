95fbaa662e9dcc26ae35798236e1304c50035073
amit <amitarnson2007@gmail.com>
2025-03-25 12:17:48 +0200

    started working on displaying song info. continuing work on a different device, hence committing

--- GUI/upload_song.py
+++ GUI/upload_song.py
@@ -33,6 +33,30 @@ def format_file_size(size_in_bytes: int) -> str:
     return f"{size:.2f} {units[-1]}"  # For extremely large sizes
 
 
+def format_length_from_milliseconds(milliseconds: int) -> str:
+    # Convert milliseconds to total seconds
+    total_seconds = milliseconds // 1000
+    seconds = total_seconds % 60
+    total_minutes = total_seconds // 60
+    minutes = total_minutes % 60
+    hours = total_minutes // 60
+
+    # Format seconds as double digits
+    seconds_str = f"{seconds:02}"
+
+    # Format minutes: double digits only if more than 9 minutes or if hours are present
+    if hours > 0 or minutes >= 10:
+        minutes_str = f"{minutes:02}"
+    else:
+        minutes_str = f"{minutes}"
+
+    # Format hours (if any): just as they are, no padding
+    if hours > 0:
+        return f"{hours}:{minutes_str}:{seconds_str}"
+    else:
+        return f"{minutes_str}:{seconds_str}"
+
+
 # This section was moved to a class due to being hard to read when inside of a function
 class ImageRemoveHover(ft.Stack):
     def __init__(self, image: ft.Image, image_name: str, width: int, height: int):
@@ -222,7 +246,8 @@ class UploadPage:
         # override the on_click method so that clicking on the button does not reset the information already input
         self.sidebar.goto_upload_song.on_click = None
 
-        # the sidebar is set to 2 and the right side of the page is set to 10. this means the sidebar takes 20% of the available screen
+        # the sidebar is set to 2 and the right side of the page is set to 10. this means the sidebar takes 20% of the
+        # available screen
         self.sidebar.expand = 2
 
         # we define 3 different file pickers so that we can point the on_result to different functions in order to make
